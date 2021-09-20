be2461bb7ba09d7c8c1ed7e087bfce43eec2b321
dzhafarovm <maksim8797@gmail.com>
2021-09-20 14:31:31 +0400

    add App hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { getImages } from './Components/PixabayApi/PixabayApi.jsx';
 import { Searchbar } from './Components/Searchbar/Searchbar.jsx';
 import { ImageGallery } from './Components/ImageGallery/ImageGallery.jsx';
@@ -7,122 +7,101 @@ import { Modal } from './Components/Modal/Modal.jsx';
 import { LoaderSpinner } from './Components/LoaderSpinner/LoaderSpinner.jsx';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    selectedImage: null,
-    page: 1,
-    arrImages: [],
-    loading: false,
-    error: false,
-  };
-
-  // --> Запрос при обновлении компонента
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchImages();
-      this.scrollTo();
-    }
-  }
-
-  nextPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [page, setPage] = useState(1);
+  const [arrImages, setArrImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   // --> Запрос и рендер изображений
-  fetchImages = () => {
-    const { searchQuery, page } = this.state;
-    const q = searchQuery;
-    const optionsApi = { q, page };
+  useEffect(() => {
+    const fetchImages = () => {
+      const q = searchQuery;
+      const optionsApi = { q, page };
 
-    this.setState(prevState => ({
-      loading: true,
-      error: false,
-    }));
+      if (q.trim() === '') {
+        return;
+      }
 
-    getImages(optionsApi)
-      .then(arrImages => {
-        if (arrImages.length > 0) {
-          this.setState(prevState => ({
-            arrImages: [...prevState.arrImages, ...arrImages],
-          }));
-        } else {
-          this.setState({ error: true });
-        }
-      })
-      .catch(() => this.setState({ error: true }))
-      .finally(() => this.setState({ loading: false }));
-  };
+      setLoading(true);
+
+      getImages(optionsApi)
+        .then(arrIm => {
+          if (arrIm.length > 0) {
+            setArrImages(s => [...s, ...arrIm]);
+            scrollTo();
+          } else {
+            setError(true);
+          }
+        })
+        .catch(() => setError(true))
+        .finally(() => setLoading(false));
+    };
+
+    fetchImages();
+  }, [searchQuery, page]);
 
   // --> Выбор src для модального окна
-  handleSelectImage = largeImage =>
-    this.setState({ selectedImage: largeImage });
+  const handleSelectImage = largeImage => setSelectedImage(largeImage);
 
   // --> Закрытие модалки
-  closeModal = e => {
+  const closeModal = e => {
     if (e.target === e.currentTarget || e.code === 'Escape') {
-      this.setState({
-        selectedImage: null,
-      });
+      setSelectedImage(null);
     }
   };
 
   // --> Скролл (GoIT)
-  scrollTo = () => {
+  const scrollTo = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  // --> Сброс параметров для нового запроса
-  updateQuery = query => {
-    this.setState({
-      searchQuery: query,
-      page: 1,
-      arrImages: [],
-    });
+  // --> Следующая страница
+  const nextPage = () => {
+    setPage(s => s + 1);
   };
 
-  render() {
-    const { searchQuery, arrImages, loading, selectedImage, error } =
-      this.state;
+  // --> Сброс параметров для нового запроса
+  const updateQuery = query => {
+    setSearchQuery(query);
+    setPage(1);
+    setArrImages([]);
+  };
 
-    const showLoadMoreButton = arrImages.length > 0 && !loading;
+  const showLoadMoreButton = arrImages.length > 0 && !loading;
 
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.updateQuery} />
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={updateQuery} />
 
-        {error && (
-          <h2 className={css.ErrorTitle}>
-            Search result '{searchQuery}' not found!
-          </h2>
-        )}
+      {error && (
+        <h2 className={css.ErrorTitle}>
+          Search result '{searchQuery}' not found!
+        </h2>
+      )}
 
-        {loading && <LoaderSpinner />}
+      <ImageGallery
+        images={arrImages}
+        query={searchQuery}
+        onSelect={handleSelectImage}
+      />
 
-        <ImageGallery
-          images={this.state.arrImages}
-          query={this.state.searchQuery}
-          onSelect={this.handleSelectImage}
+      {loading && <LoaderSpinner />}
+
+      {showLoadMoreButton && <Button onClick={nextPage} />}
+
+      {selectedImage && (
+        <Modal
+          largeImage={selectedImage}
+          alt={searchQuery}
+          onClose={closeModal}
         />
-
-        {showLoadMoreButton && <Button onClick={this.nextPage} />}
-
-        {selectedImage && (
-          <Modal
-            largeImage={selectedImage}
-            alt={searchQuery}
-            onClose={this.closeModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
